30f9c522122271ad3d556594606f27ba37de50f7
elprimo535180046 <70675036+elprimo535180046@users.noreply.github.com>
2020-12-10 21:35:30 +0700

    Added Pertemuan 13 (Vue)

---
+++ Pertemuan 13/demo_example/src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="section">
+        <div class="container is-max-desktop">
+            <h1 class="title">Login</h1>
+            <hr>
+            <b-field label="Username">
+                <b-input maxlength="30"></b-input>
+            </b-field>
+            <b-field label="Password">
+                <b-input type="password" maxlength="30"></b-input>
+            </b-field>
+            <b-button class="is-primary" expanded>Login</b-button>
+        </div>
+    </section>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Pertemuan 13/demo_example/src/components/User.vue
@@ -0,0 +1,80 @@
+<template>
+    <section class="section">
+        <div class="container is-max-desktop">
+            <h1 class="title">Users</h1>
+            <hr>
+            <div class="buttons">
+                <b-button class="is-primary" tag="router-link" to="/user-add">Add new User</b-button>
+                <b-button class="is-secondary" tag="router-link" :to="{ name:
+                'UserDetail', params: { id: 123}}">Edit User</b-button>
+                <b-button class="is-danger" @click="trashModal">Delete User</b-button>
+            </div>
+            <b-table :data="users" :columns="columns"/>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      users: [
+        {
+          'no': 1,
+          'username': 'johnthor',
+          'email': 'john.thor@gmail.com',
+          'fullname': 'Johnny Thoriq'
+        },
+        {
+          'no': 2,
+          'username': 'johnny',
+          'email': 'john.ny@gmail.com',
+          'fullname': 'Johan Nurul Qusny'
+        },
+        {
+          'no': 3,
+          'username': 'johndhela',
+          'email': 'john.dhela@gmail.com',
+          'fullname': 'Johanita Adhela'
+        }
+      ],
+      columns: [
+        {
+          field: 'no',
+          label: 'No',
+          width: '40',
+          numeric: true,
+          centered: true
+        },
+        {
+          field: 'username',
+          label: 'Username',
+          centered: true
+        },
+        {
+          field: 'email',
+          label: 'Email Address',
+          centered: true
+        },
+        {
+          field: 'fullname',
+          label: 'Full Name',
+          centered: true
+        }
+      ]
+    }
+  },
+  methods: {
+    trashModal () {
+      this.$buefy.dialog.confirm({
+        title: 'Delete Confirmation',
+        message: 'Are you sure want to delete <b>this User</b>?',
+        confirmText: 'Yes',
+        cancelText: 'Cancel',
+        type: 'is-danger',
+        onConfirm: () => {}
+      })
+    }
+  }
+}
+</script>
